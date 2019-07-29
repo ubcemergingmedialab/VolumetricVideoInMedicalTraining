9bcdba51a1f897ac127271589af7d2a2c4486e09
Mahmud <jack321@ead.ubc.ca>
2019-07-29 15:18:52 -0700

    ADded lightt

---
+++ VolumetricVideo/Assets/Main Assets/Scripts/PulsatingLight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulsatingLight : MonoBehaviour {
+
+    private Light myLight;
+    private float maxIntensity = 5f;
+    bool reachedMax = false;
+
+	// Use this for initialization
+	void Start () {
+        myLight = GetComponent<Light>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        if (!reachedMax)
+        {
+            myLight.intensity += 0.04f;
+            if (myLight.intensity >= maxIntensity)
+            {
+                reachedMax = true;
+            }
+        }
+        else
+        {
+            myLight.intensity -= 0.04f;
+            if (myLight.intensity <= 1)
+            {
+                reachedMax = false;
+            }
+        }
+         
+    }
+}
