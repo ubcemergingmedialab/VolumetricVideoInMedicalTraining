68657c8bcf445e698b23fafbe9a95a3daf0b4cec
atifmahmud <atifmahmud95@gmail.com>
2019-07-31 18:38:30 -0700

    Added new menu scene and transition

---
+++ VolumetricVideo/Assets/Main Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    public void OnButton()
+    {
+        Debug.Log("Button was pressed!");
+    }
+}
---
+++ VolumetricVideo/Assets/Main Assets/Scripts/MoveToMenuScene.cs
@@ -0,0 +1,47 @@
+﻿/// <summary>
+/// This file is meant to implement functionality of proximity based video playing 
+/// </summary>
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToMenuScene : MonoBehaviour
+{
+
+    public GameObject user;
+    private int allowedDistance = 5;
+    private Vector3 pos;
+    bool transitioned = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = gameObject.transform.position;
+        MoveToMenu(user);
+    }
+
+    /// <summary>
+    /// Checks distance between two objects and plays/pauses video based on distance
+    /// </summary>
+    /// 
+    /// <param name="objectOne">
+    /// One of the objects of interest in calculating distance with another
+    /// </param>
+
+    void MoveToMenu(GameObject objectOne)
+    {
+  
+
+        if ((Mathf.Abs(Vector3.Distance(objectOne.transform.position, pos)) <= allowedDistance) && !transitioned)
+        {
+            SceneManager.LoadScene("LevelTwo", LoadSceneMode.Additive);
+            transitioned = true;
+        }
+    }
+}
